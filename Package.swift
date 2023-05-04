dac7228f5d5b03148ecab9e730c92edc352b6454
Build Er <builder@macmini6.local>
2023-05-04 18:17:06 -0300

    Package version: 1.0.0-beta+20230504210908

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFilePicker",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFilePicker-1.0.0-beta+20230504180356.xcframework.zip",
-			checksum: "34a1f05697873754da36f9d6a74325df2513f2460199db60998b251f2a2e109e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFilePicker-1.0.0-beta+20230504210908.xcframework.zip",
+			checksum: "3d2577c4d252b82b33134be868693cd3150e58ad9c987d8777aa0f96cac02250"
 		)
 	]
 )
