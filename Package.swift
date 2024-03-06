ea478f9c6be587f366b6cf674bc00f11288d933c
genexusbot <builder@genexus.com>
2024-03-06 09:06:30 -0300

    Package version: 1.5.0-beta.12

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXFilePickerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.11")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.12")
 	],
 	targets: [
 		.target(name: "GXFilePickerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFilePicker",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFilePicker-1.5.0-beta.11.xcframework.zip",
-			checksum: "1d82fd311013bc7b9b9e4621c7a6bb9a6f07f667e5fe05a894c27bfe0d3a03ca"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFilePicker-1.5.0-beta.12.xcframework.zip",
+			checksum: "420f7bf2ff363dc36445e44ffee6931220eb81356488b5250d9a043033713428"
 		)
 	]
 )
