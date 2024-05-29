61690d9eff4e52f48bb16ae8f980fee2124688ca
Build Er <builder@macmini6.local>
2024-05-29 16:10:20 -0300

    Package version: 1.4.31

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXFilePickerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.30")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.31")
 	],
 	targets: [
 		.target(name: "GXFilePickerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFilePicker",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXFilePicker-1.4.30.xcframework.zip",
-			checksum: "8fb369716f5793e8b123b1d06982cba619cb968d66f104824ede55e119482092"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFilePicker-1.4.31.xcframework.zip",
+			checksum: "d0ed1ff1f8f24c6137bb07d319dd9b183e5f4e3089e757d0266db307a17666c5"
 		)
 	]
 )
